ca41dd05825b4ee9525ed180fc17488890527994
Nofre <davidprogram417@gmail.com>
2025-05-14 20:34:34 +0200

    chore: project initiated

---
+++ cmd/root.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:   "qube",
+	Short: "ApiQube is a powerful test manager for apps and APIs",
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
