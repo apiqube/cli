fa3f82e7d9e5a5d3a97bd894f5db4f7c083848ec
Nofre <davidprogram417@gmail.com>
2025-05-22 07:28:06 +0200

    fix(cmd): cmd arch logic restored

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/apiqube/cli/cmd/cli"
 	"github.com/apiqube/cli/internal/core/store"
 	uicli "github.com/apiqube/cli/ui/cli"
 )
@@ -13,5 +12,5 @@ func main() {
 	store.Init()
 	defer store.Stop()
 
-	cli.Execute()
+	Execute()
 }
